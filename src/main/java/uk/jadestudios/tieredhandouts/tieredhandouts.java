9061a66bd7e112770d6c43526614378c3d480dd5
NoL <na@jadestudios.uk>
2020-12-30 15:40:59 -0700

    Locsl Vault

--- src/main/java/uk/jadestudios/tieredhandouts/tieredhandouts.java
+++ src/main/java/uk/jadestudios/tieredhandouts/tieredhandouts.java
@@ -3,6 +3,7 @@ package uk.jadestudios.tieredhandouts;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -10,11 +11,16 @@ import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Collection;
+import java.util.List;
 
 
 public final class tieredhandouts extends JavaPlugin {
 
     private Economy economy;
+    private List<World> worlds;
+    private List<Double> threshold;
+    private List<Double> ubi;
+
 
     @Override
     public void onEnable() {
@@ -27,6 +33,16 @@ public final class tieredhandouts extends JavaPlugin {
         }
         this.saveDefaultConfig();
         //TODO
+
+        if(this.getConfig().isList("worlds")) {
+            worlds = (List<World>) this.getConfig().getList("worlds");
+        }
+
+        //Check if threshold = ubi - 1 and actually exists
+
+        threshold = (List<Double>) this.getConfig().getList("UBIthreshold");
+        ubi = (List<Double>) this.getConfig().getList("UBI");
+
     }
 
     //UBI in a sense
@@ -39,32 +55,37 @@ public final class tieredhandouts extends JavaPlugin {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player) sender;
-
-
-        if (player.getWorld().getName().equals(this.getConfig().getString("world"))) {
+//
+//
+//        if (player.getWorld().getName().equals(this.getConfig().getString("gamemode"))) {
 
             switch(command.getName().toLowerCase()){
                 case "run":
                     Collection<? extends Player> players = this.getServer().getOnlinePlayers();
                     for (Player s : players) {
-                        if (s.getGameMode().equals(GameMode.SURVIVAL)){
-                            if (economy.getBalance(s) < this.getConfig().getDouble("middle")) {
-                                economy.depositPlayer(s, this.getConfig().getDouble("middleCost"));
+                        if (s.getGameMode().equals(GameMode.SURVIVAL) && worlds.contains(s.getWorld())){
 
-                            } else {
-                                economy.depositPlayer(s, this.getConfig().getDouble("upperCost"));
+                            double currentBalance = economy.getBalance(s);
+
+                            if (currentBalance >= threshold.get(0)){
+                                economy.depositPlayer(s,ubi.get(0));
+                            }else if (currentBalance < threshold.get(threshold.size()-1)){
+                                economy.depositPlayer(s,ubi.get(ubi.size()-1));
                             }
 
+                            for (int i = 0; i < threshold.size()-1; i++){
+                                if (currentBalance < threshold.get(i) && currentBalance >= threshold.get(i+1)){
+                                    economy.depositPlayer(s, ubi.get(i+1));
+                                }
+                            }
                     }
 
                     }
                     player.sendMessage("Done");
             }
 
-        } else {
-            sender.sendMessage("wrong world");
-
-        }
+//        } else {
+//            sender.sendMessage("wrong world");
 
 
         return true;
